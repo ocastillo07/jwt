c1af9ebba08addb76459ae77aff2ef6a96c372eb
Oscar <oscarcastillog7@gmai.com>
2021-04-01 00:23:45 -0700

    jwt works and post controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -278,8 +278,10 @@ Devise.setup do |config|
   # change the failure app, you can configure them inside the config.warden block.
   #
   # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
+  #   # manager.intercept_401 = false
+  #   # manager.default_strategies(scope: :user).unshift :some_external_strategy
+  #   manager.strategies.add(:jwt, Devise::Strategies::JsonWebToken)
+  #   manager.default_strategies(scope: :user).unshift :jwt
   # end
 
   # ==> Mountable engine configurations
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[registrations sessions passwords]
   devise_scope :user do
   	post '/signup', to: 'registrations#create'
   	post '/login', to: 'sessions#create'
   	delete '/logout', to: 'sessions#destroy'
+  	
+  	namespace :api do
+  		namespace :v1 do
+  			resources :posts
+  		end
+  	end
   end
 end
